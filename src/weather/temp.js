ae7fe2d460c0b7615af7cd7b3ebec2bde4cfc553
Siddhant Sawant <99967026+siddhantsawant03@users.noreply.github.com>
2024-06-20 16:44:51 +0530

    Update temp.js

--- src/weather/temp.js
+++ src/weather/temp.js
@@ -7,7 +7,7 @@ const Temp = () => {
   const [tempInfo, setTempInfo] = useState("");
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=9d7f2aa62ad73e806308fa626ed30efd`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={api_key}`;
 
       let res = await fetch(url);
       let data = await res.json();
